6f84d1b232448db155a06889df44a8807af7e0f9
Gaëlle Bossard <bossard.gaelle@gmail.com>
2025-02-02 21:47:52 +0100

    Create HeaderHome composable & add profile photo

--- app/src/main/java/com/example/bookshelf/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/bookshelf/ui/screens/HomeScreen.kt
@@ -1,13 +1,63 @@
 package com.example.bookshelf.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.bookshelf.R
 
 @Composable
-fun HomeScreen() {
-    Text("Écran d'Accueil", modifier = Modifier.fillMaxSize())
+fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp)
+    ) {
+        HomeHeader(
+            modifier = Modifier
+        )
+    }
+
+}
+
+@Composable
+fun HomeHeader(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = stringResource(R.string.hello_name),
+            style = MaterialTheme.typography.displaySmall
+        )
+        Box(
+            modifier = Modifier
+        ) {
+            Image(
+                painter = painterResource(R.drawable.gaelle_cv),
+                contentDescription = stringResource(id = R.string.profile_photo),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(50.dp).clip(RoundedCornerShape(16.dp))
+            )
+        }
+    }
 }
 
 @Composable

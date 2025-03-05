9e375164585be2d1a3a7ddb0fbd1b975f43fc187
Gaëlle Bossard <bossard.gaelle@gmail.com>
2025-03-05 17:26:02 +0100

    Book: Create data class for Book

--- app/src/main/java/com/example/bookshelf/model/Book.kt
+++ app/src/main/java/com/example/bookshelf/model/Book.kt
@@ -1,25 +1,42 @@
 package com.example.bookshelf.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseData(
+    val kind: String,
+    val totalItems: Int,
+    val items: List<Book>?
+)
+
+@Serializable
 data class Book(
     val id: String,
     val volumeInfo: VolumeInfo,
     val saleInfo: SaleInfo
 )
 
+@Serializable
 data class VolumeInfo(
     val title: String,
-    val authors: List<String>,
-    val publisher: String,
+    val authors: List<String>? = listOf(),
+    val publisher: String?  = "",
     val publishedDate: String,
-    val description: String,
-    val imageLinks: ImageLinks
+    val description: String?  = "",
+    val imageLinks: ImageLinks? = null
 )
 
+@Serializable
 data class ImageLinks(
-    val smallThumbnail: String,
-    val thumbnail: String
-)
+    val smallThumbnail: String? = "",
+    val thumbnail: String? = ""
+) {
+    val httpsThumbnail: String?
+        get() = thumbnail?.replace("http", "https")
 
+}
+
+@Serializable
 data class SaleInfo(
-    val buyLink: String?
+    val buyLink: String? = ""
 )

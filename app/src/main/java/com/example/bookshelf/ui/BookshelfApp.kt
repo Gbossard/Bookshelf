29fe0076ea4825726027934147d8635c307bd701
Gaëlle Bossard <bossard.gaelle@gmail.com>
2026-03-11 15:50:44 +0100

    BookshelfApp: Update import for MainNav

--- app/src/main/java/com/example/bookshelf/ui/BookshelfApp.kt
+++ app/src/main/java/com/example/bookshelf/ui/BookshelfApp.kt
@@ -2,7 +2,7 @@ package com.example.bookshelf.ui
 
 import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
-import com.example.bookshelf.ui.composable.MainNav
+import com.example.bookshelf.ui.navigation.MainNav
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable

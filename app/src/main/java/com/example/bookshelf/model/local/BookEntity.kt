7b1b99490a5061d93705aac877f18867c3a42cdf
Gaëlle Bossard <bossard.gaelle@gmail.com>
2026-03-25 15:58:54 +0100

    BookEntity: Create new Book Entity

---
+++ app/src/main/java/com/example/bookshelf/model/local/BookEntity.kt
@@ -0,0 +1,20 @@
+package com.example.bookshelf.model.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "book_table")
+data class BookEntity (
+    @PrimaryKey
+    val id: String,
+    val title: String,
+    val authors: String?,
+    val publishers: String?,
+    val publishedDate: String?,
+    val description: String?,
+    val pageCount: Int?,
+    val thumbnail: String?,
+    val isbn: String?,
+    val buyLink: String?,
+    val isFavorite: Boolean = false
+)
